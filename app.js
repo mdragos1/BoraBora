dd6c12b413808311d4b793cb4e95811f35219837
Dragos Stefan Mihalcea <dragos.mihalcea@s.unibuc.ro>
2022-05-18 18:12:07 +0300

    node si commit-uri cu sens

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const formidable=require('formidable');
 
 let reservation=[];
 
@@ -47,6 +49,34 @@ app.post('/contact', function (req, res) {
         res.render('template.ejs' ,{name, timeDays});
     }
    
+});
+
+app.post('/signup',function(req,res){
+    let userDetails;
+    console.log(fs.existsSync("accounts.json"))
+  if (fs.existsSync("accounts.json")){
+      console.log('exista');
+    let data = fs.readFileSync("accounts.json");
+    userDetails=JSON.parse(data);
+  }else{
+    userDetails=[];
+  }
+
+  const details = {email, password} = req.body;
+  //   userDetails.forEach(element => {
+  //       console.log(element[0]);
+  //   });
+  userDetails.push(details);
+  fs.writeFile('accounts.json',JSON.stringify(userDetails),(err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("JSON data is saved.");
+  });
+  let name = email;
+  let timeDays = password;
+  res.render('template.ejs' ,{name, timeDays});
+//   res.render('Rezervari_realizate.ejs' ,{firstname, lastname, email, nr, data_sosire, data_plecare, camera, guests, requests});
 });
 
 app.use('/css', express.static(path.join(__dirname)));
